b3bae580a079d3cef6e1d61d29bfb0746fffaf69
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-05-07 15:31:12 +0200

    fix: update junit dependencies and enable useJUnitPlatform

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,9 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation(platform("org.junit:junit-bom:5.12.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.25.3")
 }
 
@@ -54,6 +54,7 @@ tasks {
         }
     }
     test {
+        useJUnitPlatform()
         testLogging {
             events("passed", "skipped", "failed")
         }
